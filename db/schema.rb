96701abeb7757d97414225b3e311c1b0f914bcb9
iivan <iivan@pitechnologies.ro>
2014-07-24 17:32:11 +0300

    initial model for messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716144236) do
+ActiveRecord::Schema.define(version: 20140724140956) do
+
+  create_table "messages", force: true do |t|
+    t.integer  "sender"
+    t.integer  "receiver"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", force: true do |t|
     t.string   "content"
