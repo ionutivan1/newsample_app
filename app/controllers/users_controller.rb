170bfbdf9d0f1c92af7ef567c9a35b3534d93a1e
iivan <iivan@pitechnologies.ro>
2014-07-29 18:06:06 +0300

    @user.activate! requires a valid password for some reason

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,34 +43,15 @@ before_action :find_user
         redirect_to signin_path
         flash[:success] = "Account confirmed"
 
-<<<<<<< HEAD
       respond_to do |format|
         UserMailer.welcome_mail(@user).deliver
         sign_in @user
         format.html { redirect_to @user }
         flash[:success] = "Welcome to the Sample App!"
-
-=======
-        @user.confirmation = @user.sign_up_token
->>>>>>> Finished modifications to signup confirmation
-        if @user.save
-        UserMailer.welcome_mail(@user).deliver
-        respond_to do |format|
-          format.html { redirect_to root_url }
-          format.js
-          flash[:success] = "Check email for confirmation link"
-        end
-<<<<<<< HEAD
       end
     else
       render 'new'
     end
-=======
-          else
-            render 'new'
-          end
-
->>>>>>> Finished modifications to signup confirmation
   end
 
   def set_complete
@@ -106,26 +87,16 @@ before_action :find_user
 
   def following
     @title = "Following"
-<<<<<<< HEAD
     followers_or_following(params[:id], params[:page])
-=======
-    @users = @user.followed_users.paginate(page: params[:page])
-    render 'show_follow'
->>>>>>> Finished modifications to signup confirmation
   end
 
   def followers
     @title = "Followers"
-<<<<<<< HEAD
     followers_or_following(params[:id], params[:page])
   end
 
   def notifications
    render 'notifications'
-=======
-    @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
->>>>>>> Finished modifications to signup confirmation
   end
 
   private
@@ -154,7 +125,6 @@ before_action :find_user
 
   def find_user
     if params[:id]
-<<<<<<< HEAD
       @user = User.find_by_id(params[:id])
       if @user.blank?
         redirect_to signin_url, notice: "Bad url."
@@ -167,11 +137,4 @@ before_action :find_user
     @users = @user.followed_users.paginate(page: page)
     render 'show_follow'
   end
-=======
-    @user = User.find(params[:id])
-    else
-      @user = User.new
-    end
-  end
->>>>>>> Finished modifications to signup confirmation
 end
