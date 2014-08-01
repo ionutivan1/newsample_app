9d301f239e8c733aaf2d74f11f35448e89cb5035
iivan <iivan@pitechnologies.ro>
2014-08-01 17:32:10 +0300

    basic private messaging system

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,8 @@ class Message < ActiveRecord::Base
 
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
-  validates :sender, presence: true
-  validates :receiver, presence: true
+  validates :user_id, presence: true
+  validates :sender_id, presence: true
 
   # def self.get_receiver(user)
   #   receiver_id = "SELECT receiver FROM messages
