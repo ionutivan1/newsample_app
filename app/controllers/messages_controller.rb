f9e614c2ebf69f20e8ec127cfcd9d5ff6edf4976
iivan <iivan@pitechnologies.ro>
2014-08-06 12:08:51 +0300

    messaging with proper inbox

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,14 +10,23 @@ class MessagesController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @messages = @user.messages.paginate(page: params[:page])
   end
 
   def create
     @message = Message.new(message_params)
     @message.sender_id = current_user.id
-    @message.user_id = User.find_by_email(params[:message][:user_id]).id
+    if params[:message][:user_id].blank?
+      flash[:notice] = "Can't send it to no one! Input an email address please."
+      redirect_to message_url(current_user) and return
+    else
+      @message.user_id = User.find_by_email(params[:message][:user_id]).id
+    end
+    if params[:message][:content].blank?
+      flash[:notice] = "Content cannot be empty."
+      redirect_to message_url(current_user) and return
+    end
     if @message.save
       flash[:success] = "Message sent!"
       redirect_to message_url(current_user)
