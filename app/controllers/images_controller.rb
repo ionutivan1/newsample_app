1c6fdb5d88334150bb8b3066db209188e6b12906
iivan <iivan@pitechnologies.ro>
2014-09-29 16:47:47 +0300

    basic files added

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,35 @@
-class ImagesController < Application
+class ImagesController < ApplicationController
+
+  before_action :start_update, only: [:update]
+
+  def index
+
+  end
+
+  def new
+  end
+
+  def update
+    @a = @image_update.updateImage
+    render 'show'
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:element, :element_id, :image)
+  end
+
+  def start_update
+    @image_update = ImageService::ImageUpdateService.new(params)
+  end
+
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,7 @@
-class Image
-
+class Image < ActiveRecord::Base
+  validates :element, presence: true
+  validates :element_id, presence: true
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => /\Aimage/
+  validates_attachment_file_name :image, :matches => [/gif\Z/, /jpe?g\Z/]
 end
