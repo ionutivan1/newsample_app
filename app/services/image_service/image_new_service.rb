c80384a407827c1aa435e6699bf1b2446552c48a
iivan <iivan@pitechnologies.ro>
2014-09-29 16:49:16 +0300

    started work on services

--- app/services/image_service/image_new_service.rb
+++ app/services/image_service/image_new_service.rb
@@ -1,2 +1,18 @@
-class ImageNewService
+module ImageService
+  class ImageNewService
+    def initialize(params)
+      params = params
+    end
+
+    def newImage
+      # puts "aaaaaaaaaaaaaaaaaaaaaaaaaa"
+      # puts params
+      # params.inspect
+      # puts "aaaaaaaaaaaaaaaaaaaaaaaaaa"
+      return "blabla new image"
+    end
+
+    private
+
+  end
 end
--- app/services/image_service/image_update_service.rb
+++ app/services/image_service/image_update_service.rb
@@ -1,14 +1,23 @@
 module ImageService
   class ImageUpdateService
     def initialize(params)
-      params = params
+      @image_params = params[:user]
+      @element = params[:user][:element]
+      @element_id = params[:id]
     end
 
     def updateImage
-
+      gettheimage = find_image
+      update(gettheimage)
     end
+
     private
+    def find_image
+      Image.find_by(element: @element, element_id: @element_id)
+    end
 
-
+    def update(bla)
+        bla.update_params(@image_params)
+    end
   end
 end
