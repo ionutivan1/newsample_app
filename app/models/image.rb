1a95300a317995d6ff99f8260ced2c265a40d8d6
iivan <iivan@pitechnologies.ro>
2014-10-08 12:04:48 +0300

    added paperclip interpolation to add support for micropost pictures

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,17 +2,20 @@ class Image < ActiveRecord::Base
   validates :element, presence: true
   validates :element_id, presence: true
   has_attached_file :image,
-    :url => "/upload/profile/:id/:basename.:extension",
+    :url => "/upload/:element/:id/:basename.:extension",
     :styles => {
     :thumb => "50x50",
     :small => "150x150",
     :medium => "200x200",
     },
-    :path => ":rails_root/public/upload/profile/:id/:basename.:extension",
+    :path => ":rails_root/public/upload/:element/:id/:basename.:extension",
     :default_url => ":rails_root/public/upload/profile/default.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage/
   validates_attachment_file_name :image, :matches => [/gif\Z/, /jpe?g\Z/]
 
+  Paperclip.interpolates :element do |image,style|
+    image.instance.element
+  end
   def self.profile_picture(user)
     find_by(element: "user", element_id: user.id)
   end
